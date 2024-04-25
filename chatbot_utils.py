8d9d99ddcdbe43d3d1781b9f783e01755554786a
Manoj <129957999+ManojP96@users.noreply.github.com>
2024-04-25 14:04:55 +0530

    Update chatbot_utils.py

--- chatbot_utils.py
+++ chatbot_utils.py
@@ -14,7 +14,9 @@ class BedrockLLM:
             'bedrock',
             region_name=AWS_REGION,
             aws_access_key_id=ACCESS_KEY,
-            aws_secret_access_key=SECRET_ACCESS_KEY
+            aws_secret_access_key=SECRET_ACCESS_KEY,
+            aws_session_token="IQoJb3JpZ2luX2VjEFgaCXVzLWVhc3QtMSJIMEYCIQCyOaIrHCax2JTb+5IJoFGps/mLSK+TG7hWQKPxdIlzrAIhAMIxeBikXE4yfBhQdDhFQH0dWi6A/aO3mnSomUi9mtFJKo8DCKH//////////wEQABoMNDcxMTEyOTY3MjIwIgzLLy2pkiU+o5M8Q3Qq4wLWTX34XQCsCiOc1Z69fCuAg0Pu0nZvpxSY9uVRBFeGqC+oqoJIUS3YPNDbGqrhOHB/6xLfXalVMl7gQc5nHcZrQEjhUGhn/9/ZSAcP7wKQdLZqjRtbqdnB3GteaxSUaH77OIabpJmwb9MMf3socZdxeBBmgXCXcid6sG5MXOI8l4CeC4XwHZqb1nVcEhsoVgXem96nWmwHuAHIEeBg4HN2kwl/pnQE6LhGoik43uHPsLOw82XDE9ABZ4HIYxtJNZLI6MsR9nxM69Q2Nl3iqg5WqYPoP1za45AfYfjL3UqNn/0BZnIZULcxINuPiH/u/1SBfPfSijF61SvsDUq177vm44FwaDA+jQdHL9aFYZ0YjGUjS/9TKjMqaWGBHa0MjpW2rjLqg0L65+3N7djtn9jNUQpm20r5hJaX1UUoHjwds1WVTFmCZ9ckdUeHDTM29F/svkHIQxrsJwF9zisgWnKqlBaNMKSSqLEGOqUBx19pV4TEdu5jWRduX8gaQu8Y7pb11JJwPQzjl57qyz/LevPZWrxm3xdXiSabaOKzzkXKXLZG2x87OcjmYhk58RVLmJ6zaEq2cNfJBFaiJkOHMvkEWhZ4jLh7SLal4/ZZybTyw0jjqMUR3ui8JQUt5MmY9Ms7p1CBJvIcCUY7Ea8cV2Xt9RdVqfJVj9ort0NyKghKU6lDlHOVrP5owgHfJEIiH9tT"
+
         )
 
         return bedrock_client
@@ -28,8 +30,9 @@ class BedrockLLM:
             'bedrock-runtime',
             region_name=AWS_REGION,
             aws_access_key_id=ACCESS_KEY,
-            aws_secret_access_key=SECRET_ACCESS_KEY
+            aws_secret_access_key=SECRET_ACCESS_KEY,
+            aws_session_token="IQoJb3JpZ2luX2VjEFgaCXVzLWVhc3QtMSJIMEYCIQCyOaIrHCax2JTb+5IJoFGps/mLSK+TG7hWQKPxdIlzrAIhAMIxeBikXE4yfBhQdDhFQH0dWi6A/aO3mnSomUi9mtFJKo8DCKH//////////wEQABoMNDcxMTEyOTY3MjIwIgzLLy2pkiU+o5M8Q3Qq4wLWTX34XQCsCiOc1Z69fCuAg0Pu0nZvpxSY9uVRBFeGqC+oqoJIUS3YPNDbGqrhOHB/6xLfXalVMl7gQc5nHcZrQEjhUGhn/9/ZSAcP7wKQdLZqjRtbqdnB3GteaxSUaH77OIabpJmwb9MMf3socZdxeBBmgXCXcid6sG5MXOI8l4CeC4XwHZqb1nVcEhsoVgXem96nWmwHuAHIEeBg4HN2kwl/pnQE6LhGoik43uHPsLOw82XDE9ABZ4HIYxtJNZLI6MsR9nxM69Q2Nl3iqg5WqYPoP1za45AfYfjL3UqNn/0BZnIZULcxINuPiH/u/1SBfPfSijF61SvsDUq177vm44FwaDA+jQdHL9aFYZ0YjGUjS/9TKjMqaWGBHa0MjpW2rjLqg0L65+3N7djtn9jNUQpm20r5hJaX1UUoHjwds1WVTFmCZ9ckdUeHDTM29F/svkHIQxrsJwF9zisgWnKqlBaNMKSSqLEGOqUBx19pV4TEdu5jWRduX8gaQu8Y7pb11JJwPQzjl57qyz/LevPZWrxm3xdXiSabaOKzzkXKXLZG2x87OcjmYhk58RVLmJ6zaEq2cNfJBFaiJkOHMvkEWhZ4jLh7SLal4/ZZybTyw0jjqMUR3ui8JQUt5MmY9Ms7p1CBJvIcCUY7Ea8cV2Xt9RdVqfJVj9ort0NyKghKU6lDlHOVrP5owgHfJEIiH9tT"
+
         )
 
         return bedrock_runtime_client
-
